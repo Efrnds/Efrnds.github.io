63973dc8e6dd9c791aa6ad070e7efe4b8b95898f
Eduardo fernandes da silva <eduardo.fernandes.silva13@gmail.com>
2023-02-08 16:18:46 -0300

    asft2

--- projects/camera.tools/script.js
+++ projects/camera.tools/script.js
@@ -1,14 +1,11 @@
 function fun1() {
     if (document.getElementById("onoff1").checked == true) {
-        document.getElementById("demo1").innerHTML = "ON   ";
+        document.getElementById("demo1").innerHTML = "ON";
     } else {
         document.getElementById("demo1").innerHTML = "OFF";
     }
-};
-
-function fun2() {
     if (document.getElementById("onoff2").checked == true) {
-        document.getElementById("demo2").innerHTML = "ON   ";
+        document.getElementById("demo2").innerHTML = "ON ";
     } else {
         document.getElementById("demo2").innerHTML = "OFF";
     }
@@ -17,4 +14,33 @@ function fun2() {
 function load() {
     document.getElementById("demo1").innerHTML = "OFF";
     document.getElementById("demo2").innerHTML = "OFF";
-}
+};
+
+function fun2() {
+    let btn1 = document.querySelector("#btn-1");
+    let btn2 = document.querySelector("#btn-2");
+    let btn3 = document.querySelector("#btn-3");
+    let btn4 = document.querySelector("#btn-4");
+    let btn5 = document.querySelector("#btn-5");
+
+    btn1.addEventListener('click', () =>{
+        document.body.style.backgroundImage = "url('img/Control.png')";
+    });
+
+    btn2.addEventListener('click', () =>{
+        document.body.style.backgroundImage = "url('img/Forza.png')";
+    });
+
+    btn3.addEventListener('click', () =>{
+        document.body.style.backgroundImage = "url('img/Sayonara.png')";
+    });
+
+    btn4.addEventListener('click', () =>{
+        document.body.style.backgroundImage = "url('img/Control.png')";
+    });
+
+    btn5.addEventListener('click', () =>{
+        document.body.style.backgroundImage = "url('img/Control.png')";
+    });
+};
+
